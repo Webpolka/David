4cb90ca7f70abcf0c86c71a180ecd11db51e2903
Webpolka <webpolka@gmail.com>
2025-06-25 19:27:42 +0300

    Second commit

--- javascript/main.js
+++ javascript/main.js
@@ -25,60 +25,82 @@ window.addEventListener("DOMContentLoaded", function () {
 Swiper coverFlow 
 ------------------------------------------------------------------------------------------------------------------------------------------------------*/
 
-  const swiperContainer = document.querySelector("#swiper");
-  if (swiperContainer) {
-    const swiper = new Swiper("#swiper", {
-      // allowTouchMove: false,
-      loop: true,
-      effect: "coverflow",
-      grabCursor: true,
-      centeredSlides: true,
-      slidesPerView: 3,
-      coverflowEffect: {
-        rotate: 0,
-        stretch: 0,
-        depth: 120,
-        modifier: 4,
-        slideShadows: false,
-      },
-    });
-    swiperContainer.addEventListener("click", function () {
-      swiper.slideNext(); // Переключаемся на следующий слайд при клике
-    });
+ const swiperContainer = document.querySelector("#swiper");
+if (swiperContainer) {
+	const swiper = new Swiper("#swiper", {
+		// allowTouchMove: false,
+		loop: true,
+		effect: "coverflow",
+		grabCursor: true,
+		centeredSlides: true,
+		slidesPerView: 3,
+		coverflowEffect: {
+			rotate: 0,
+			stretch: 0,
+			depth: 120,
+			modifier: 4,
+			slideShadows: false,
+		},
+		breakpoints: {
+			0: {
+				slidesPerView: 2,
+				coverflowEffect: {
+					rotate: 0,
+					stretch: 0,
+					depth: 100,
+					modifier: 4,
+					slideShadows: false,
+				},
+			},
+			576: {
+				slidesPerView: 3,
+				coverflowEffect: {
+					rotate: 0,
+					stretch: 0,
+					depth: 120,
+					modifier: 4,
+					slideShadows: false,
+				},
+			},
+		},
+	});
+	swiperContainer.addEventListener("click", function () {
+		swiper.slideNext(); // Переключаемся на следующий слайд при клике
+	});
 
-    const eventsContents = document.querySelector("#events-contents");
-    if (eventsContents) {
-      const cont1 = document.querySelector("#content-1");
-      const cont2 = document.querySelector("#content-2");
-      const cont3 = document.querySelector("#content-3");
-      const cont4 = document.querySelector("#content-4");
-      const cont5 = document.querySelector("#content-5");
-      const allContents = [cont1, cont2, cont3, cont4, cont5];
+	const eventsContents = document.querySelector("#events-contents");
+	if (eventsContents) {
+		const cont1 = document.querySelector("#content-1");
+		const cont2 = document.querySelector("#content-2");
+		const cont3 = document.querySelector("#content-3");
+		const cont4 = document.querySelector("#content-4");
+		const cont5 = document.querySelector("#content-5");
+		const allContents = [cont1, cont2, cont3, cont4, cont5];
 
-      swiper.on("slideChange", function () {
-        const activeIndex = swiper.activeIndex;
-        const nextSlideIndex = activeIndex + 1;
-        const nextSlide = swiper.slides[nextSlideIndex];
+		swiper.on("slideChange", function () {
+			const activeIndex = swiper.activeIndex;
+			const nextSlideIndex = activeIndex + 1;
+			const nextSlide = swiper.slides[nextSlideIndex];
 
-        showContent();
-        function showContent() {
-          var numberPattern = /\d+/g;
+			showContent();
+			function showContent() {
+				var numberPattern = /\d+/g;
 
-          const nextId = nextSlide.getAttribute("id");
-          const id = Number(nextId.match(numberPattern).join(""));
+				const nextId = nextSlide.getAttribute("id");
+				const id = Number(nextId.match(numberPattern).join(""));
 
-          allContents.forEach((cont) => {
-            const d = cont.getAttribute("id");
-            const contId = Number(d.match(numberPattern).join(""));
+				allContents.forEach((cont) => {
+					const d = cont.getAttribute("id");
+					const contId = Number(d.match(numberPattern).join(""));
 
-            if (contId == id) {
-              cont.classList.add("show");
-            } else {
-              cont.classList.remove("show");
-            }
-          });
-        }
-      });
-    }
-  }
+					if (contId == id) {
+						cont.classList.add("show");
+					} else {
+						cont.classList.remove("show");
+					}
+				});
+			}
+		});
+	}
+}
 });
